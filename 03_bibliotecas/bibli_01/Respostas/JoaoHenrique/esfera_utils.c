777f3d6961a08015b088ae0c734dfdfb89575b91
jhvlima <joao.h.lima@edu.ufes.br>
2023-08-31 14:54:54 -0300

    exer esfera

--- 03_bibliotecas/bibli_01/Respostas/JoaoHenrique/esfera_utils.c
+++ 03_bibliotecas/bibli_01/Respostas/JoaoHenrique/esfera_utils.c
@@ -2,7 +2,7 @@
 
 float calcula_volume(float R)
 {
-    return (3*PI*R*R*R)/4;
+    return (4*PI*R*R*R)/3;
 }
 
 float calcula_area (float R)
--- 03_bibliotecas/bibli_01/Respostas/JoaoHenrique/main.c
+++ 03_bibliotecas/bibli_01/Respostas/JoaoHenrique/main.c
@@ -7,6 +7,6 @@ int main()
     scanf("%f", &raio);
     area = calcula_area(raio);
     volume = calcula_volume(raio);
-    printf("Area:%f\nVolume:%f", area, volume);
+    printf("Area:%.2f\nVolume:%.2f\n", area, volume);
     return 0;
 }
