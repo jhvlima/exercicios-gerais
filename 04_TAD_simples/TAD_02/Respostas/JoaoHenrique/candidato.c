6c7dde8c7f65667704f2e4e5f5ed51eeece19fea
jhvlima <joao.h.lima@edu.ufes.br>
2023-09-19 11:25:51 -0300

    inicio tad 2

---
+++ 04_TAD_simples/TAD_02/Respostas/JoaoHenrique/candidato.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include "candidato.h"
+/**
+ * @brief Cria um candidato com os dados informados.
+ * @param nome Nome do candidato.
+ * @param partido Partido do candidato.
+ * @param cargo Cargo do candidato.
+ * @param id Identificador do candidato.
+ * @return Candidato criado.
+ */
+tCandidato CriaCandidato(char *nome, char *partido, char cargo, int id)
+{
+    tCandidato candidato;
+    candidato.nome = nome;
+    canditado.partido = partido;
+    candidato.cargo = cargo;
+    candidato.id = id;
+    return candidato;
+}
+
+/**
+ * @brief Lê um candidato.
+ * @return Candidato lido.
+ */
+tCandidato LeCandidato()
+{
+    scanf("");
+    CriaCandidato(nome, partido, cargo, id);
+    return candidato;
+}
+
+/**
+ * @brief Verifica se o identificador do candidato é igual ao informado.
+ * @param candidato Candidato a ser verificado.
+ * @param id Identificador a ser comparado.
+ * @return 1 se o identificador é igual, 0 caso contrário.
+ */
+int VerificaIdCandidato(tCandidato candidato, int id)
+{
+    if (canditado.id == id)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief Verifica se dois candidatos são iguais (dica: utilize para verificar se houve empate).
+ * @param candidato1 Primeiro candidato a ser comparado.
+ * @param candidato2 Segundo candidato a ser comparado.
+ * @return 1 se os candidatos são iguais, 0 caso contrário.
+ */
+int EhMesmoCandidato(tCandidato candidato1, tCandidato candidato2)
+{
+    if (candidato1.id == candidato2.id)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief Obtém o cargo do candidato.
+ * @param candidato Candidato a ser verificado.
+ * @return Cargo do candidato.
+ */
+char ObtemCargo(tCandidato candidato)
+{
+    return candidato.cargo;
+}
+
+/**
+ * @brief Incrementa 1 voto ao candidato.
+ * @param candidato Candidato a ter os votos incrementados.
+ * @return Candidato com a quantidade de votos incrementada.
+ */
+tCandidato IncrementaVotoCandidato(tCandidato candidato)
+{
+    candidato.votos++;
+    return candidato;
+}
+
+/**
+ * @brief Obtém a quantidade de votos do candidato.
+ * @param candidato Candidato a ser verificado.
+ * @return Quantidade de votos do candidato.
+ */
+int ObtemVotos(tCandidato candidato)
+{
+    return candidato.votos;
+}
+
+/**
+ * @brief Calcula o percentual de votos do candidato em relação ao total de votos (do mesmo cargo).
+ * @param candidato Candidato a ser verificado.
+ * @param totalVotos Total de votos da eleição.
+ * @return Percentual de votos do candidato.
+ */
+float CalculaPercentualVotos(tCandidato candidato, int totalVotos)
+{
+    return (100*(candidato.votos))/totalVotos;
+}
+
+/**
+ * @brief Imprime os dados do candidato na tela.
+ * @param candidato Candidato a ser impresso.
+ * @param percentualVotos Percentual de votos do candidato.
+ */
+void ImprimeCandidato (tCandidato candidato, float percentualVotos)
+{
+    if (candidato.cargo == presidente)
+    {
+        printf("- PRESIDENTE ELEITO: %s %s, %d voto(s), %f", candidato.nome, candidato.partido, candidato.votos, percentualVotos);
+    }
+    else
+    {
+        printf("- GOVERNADOR ELEITO: %s %s, %d voto(s), %f", candidato.nome, candidato.partido, candidato.votos, percentualVotos);
+    }
+    
+}
