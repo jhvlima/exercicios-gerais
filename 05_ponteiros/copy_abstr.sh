a4bd258190768d2b1cb2e4e17528513bee6fc1ea
jhvlima <joao.h.lima@edu.ufes.br>
2023-09-30 17:58:09 -0300

    ponteiros 3

---
+++ 05_ponteiros/copy_abstr.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Define source and destination directories and filenames
+source_dir="pont_01"
+source_file="correcao.sh"
+include_dir="Includes"
+output_dir="Respostas/JoaoHenrique"
+main_file="main.c"
+
+# Define the range of destination directories
+start_dest=3
+end_dest=13
+
+# Loop through destination directories
+for ((dest_dir = start_dest; dest_dir <= end_dest; dest_dir++)); do
+  # Define the full paths for source and destination
+  source_path="$source_dir/$source_file"
+  dest_path="pont_${dest_dir}/$output_dir"
+
+  # Check if the destination directory exists
+  if [ -d "$dest_path" ]; then
+    # Create the Respostas/JoaoHenrique directory within the destination directory
+    mkdir -p "$dest_path"
+
+    # Copy the contents of the Include directory to Respostas/JoaoHenrique
+    find "$dest_dir/$include_dir" -type f -exec cp {} "$dest_path/" \;
+
+    # Rename the copied files in the Respostas/JoaoHenrique directory from .h to .c
+    for file in "$dest_path"/*.h; do
+      file_deconversao="$file.tmp"
+      cp "$file" "$file_deconversao"
+      mv "$file_deconversao" "${file%.h}.c"
+    done
+
+    # Create a main.c file in the Respostas/JoaoHenrique directory
+    echo "// Your C code goes here" > "$dest_path/$main_file"
+
+    # Copy the source file to the destination directory
+    cp "$source_path" "pont_${dest_dir}/"
+
+    echo "Copied '$source_file' and '$include_dir' contents to 'pont_${dest_dir}/'"
+  else
+    echo "Destination directory 'pont_${dest_dir}/$output_dir' does not exist. Skipping."
+  fi
+done
